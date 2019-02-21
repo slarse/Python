c98715c13966f676062a2e833493cf86e4b917ef
Simon Larsén <slarse@kth.se>
2019-02-21 18:32:45 +0100

    Update test_imports to only load modules from thealgorithms package

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -3,6 +3,8 @@ import re
 import importlib
 import pytest
 
+import thealgorithms
+
 
 def _split_path_rec(path):
     rest, tail = os.path.split(path)
@@ -15,23 +17,22 @@ def _gather_modules():
     """Gather all modules that should be imported."""
     testdir = os.path.dirname(__file__)
     root = os.path.dirname(testdir)
-    excludedirs = [os.path.abspath(testdir)]
+    algos_root = os.path.dirname(thealgorithms.__file__)
     modules = []
 
-    for dirpath, dirnames, filenames in os.walk(root, testdir):
-        dirnames[:] = [d for d in dirnames if not os.path.abspath(d) in excludedirs]
+    for dirpath, dirnames, filenames in os.walk(algos_root):
         for filename in filenames:
             if not filename.endswith(".py"):
                 continue
-            reldirpath = os.path.relpath(dirpath, testdir)
+            reldirpath = os.path.relpath(dirpath, root)
             path_nodes = _split_path_rec(os.path.join(reldirpath, filename))
-            module = ".".join(path_nodes[1:])[:-3]
+            module = ".".join(path_nodes)[:-3]
             modules.append(module)
     return modules
 
 
 @pytest.mark.parametrize("module", _gather_modules())
-@pytest.mark.timeout(0.15) # must be larger than 0.1
+@pytest.mark.timeout(0.3)
 def test_import_module(module):
     """Test that all modules from project root can be imported"""
     mod = importlib.import_module(module)
