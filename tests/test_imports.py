b40819335bc3a570fed2e7c28992c6790ec7aa7c
Simon Larsén <slarse@kth.se>
2019-02-21 13:30:47 +0100

    Update test_imports to try all imports and handle timeouts

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,21 +1,23 @@
 import os
 import re
 import importlib
+import pytest
 
 
 def _split_path_rec(path):
     rest, tail = os.path.split(path)
-    if rest == '':
+    if rest == "":
         return [tail]
     return _split_path_rec(rest) + [tail]
-    
 
-def test_all_modules_can_be_imported():
-    """test that all modules from project root can be imported"""
+
+def _gather_modules():
+    """Gather all modules that should be imported."""
     testdir = os.path.dirname(__file__)
     root = os.path.dirname(testdir)
     excludedirs = [os.path.abspath(testdir)]
-    
+    modules = []
+
     for dirpath, dirnames, filenames in os.walk(root, testdir):
         dirnames[:] = [d for d in dirnames if not os.path.abspath(d) in excludedirs]
         for filename in filenames:
@@ -23,5 +25,14 @@ def test_all_modules_can_be_imported():
                 continue
             reldirpath = os.path.relpath(dirpath, testdir)
             path_nodes = _split_path_rec(os.path.join(reldirpath, filename))
-            s = ".".join(path_nodes[1:])[:-3]
-            importlib.import_module(s)
+            module = ".".join(path_nodes[1:])[:-3]
+            modules.append(module)
+    return modules
+
+
+@pytest.mark.parametrize("module", _gather_modules())
+@pytest.mark.timeout(0.15) # must be larger than 0.1
+def test_import_module(module):
+    """Test that all modules from project root can be imported"""
+    mod = importlib.import_module(module)
+    assert mod
