0f66066f80a59184fd38d08961c3c40848b253ec
Simon Larsén <slarse@kth.se>
2019-02-22 10:51:39 +0100

    Add known required packages

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", mode="r") as file:
     README = file.read()
 
 TEST_REQUIREMENTS = ["pytest>=4.0.0", "pytest-cov", "pytest-timeout"]
-REQUIRED = ["numpy"]
+REQUIRED = ["numpy", "matplotlib", "sympy", "scikit-learn", "tensorflow"]
 
 setup(
     name="TheAlgorithms",
