3a9f9bd6ce76922c651bbc8f0671c56b631ea95b
Simon Larsén <slarse@kth.se>
2019-02-22 10:32:16 +0100

    Increase timeout for import tests to 1 second

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -34,7 +34,7 @@ def _gather_modules():
 
 
 @pytest.mark.parametrize("module", _gather_modules())
-@pytest.mark.timeout(0.3)
+@pytest.mark.timeout(1)
 def test_import_module(module):
     """Test that all modules from project root can be imported"""
     mod = importlib.import_module(module)
