595c9a6a76ba02fdd026037a5b3c8422c52e4c37
rouwaydhanna <rouwayd@hotmail.com>
2019-02-23 13:07:55 +0100

    Make simple_client importable (#39)

--- thealgorithms/simple_client/server.py
+++ thealgorithms/simple_client/server.py
@@ -2,20 +2,24 @@
 
 import socket
 
-HOST, PORT = '127.0.0.1', 1400
+def main():
+	HOST, PORT = '127.0.0.1', 1400
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#refer to client.py
-s.bind((HOST, PORT))
-s.listen(1)#listen for 1 connection
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#refer to client.py
+	s.bind((HOST, PORT))
+	s.listen(1)#listen for 1 connection
 
-conn, addr = s.accept()#start the actual data flow
+	conn, addr = s.accept()#start the actual data flow
 
-print('connected to:', addr)
+	print('connected to:', addr)
 
-while 1:
-    data = conn.recv(1024).decode('ascii')#receive 1024 bytes and decode using ascii
-    if not data:
-        break
-    conn.send((data + ' [ addition by server ]').encode('ascii'))
+	while 1:
+	    data = conn.recv(1024).decode('ascii')#receive 1024 bytes and decode using ascii
+	    if not data:
+	        break
+	    conn.send((data + ' [ addition by server ]').encode('ascii'))
 
-conn.close()
+	conn.close()
+
+if __name__ == '__main__':
+	main()
