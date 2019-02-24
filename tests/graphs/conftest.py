677c65a049119a0150bb573b07866f5d795feeb6
larshum <larshum@kth.se>
2019-02-24 14:25:03 +0100

    Add pytest tests to graphs/

---
+++ tests/graphs/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+@pytest.fixture
+def sample_graph():
+    return {"A": ["B", "C"],
+            "B": ["A", "D", "E"],
+            "C": ["A", "F"],
+            "D": ["B"],
+            "E": ["B", "F"],
+            "F": ["C", "E"]}
---
+++ tests/graphs/test_articulation_points.py
@@ -0,0 +1,20 @@
+import pytest
+
+from graphs.articulation_points import computeAP
+
+class TestArticulationPoints:
+    @pytest.fixture
+    def adjacency_list_graph(self):
+        return {0: [1, 2],
+                1: [0, 2],
+                2: [0, 1, 3, 5],
+                3: [2, 4],
+                4: [3],
+                5: [2, 6, 8],
+                6: [5, 7],
+                7: [6, 8],
+                8: [5, 7]}
+
+
+    def test_articulation_points(self, adjacency_list_graph):
+        assert sorted(computeAP(adjacency_list_graph)) == [2, 3, 5]
