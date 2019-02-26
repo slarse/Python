f673eba7395e9ca0db98be60edf0b4b7b1a091c3
mudaliarkartik <mudaliarkartik@yahoo.com>
2019-02-26 13:21:25 +0100

    Remove docstrings from test_rabin_karp

--- tests/strings/test_rabin_karp.py
+++ tests/strings/test_rabin_karp.py
@@ -2,7 +2,6 @@ from strings.rabin_karp import rabin_karp
 
 
 def test_rabin_karp_find_pattern_in_text1_and_text2():
-    """Testing for pattern matching"""
     pattern = "abc1abc12"
     text1 = "alskfjaldsabc1abc1abc12k23adsfabcabc"
     text2 = "alskfjaldsk23adsfabcabc"
@@ -10,21 +9,18 @@ def test_rabin_karp_find_pattern_in_text1_and_text2():
 
 
 def test_rabin_karp_find_pattern_in_text_1():
-    """Testing for pattern matching"""
     pattern = "ABABX"
     text = "ABABZABABYABABX"
     assert rabin_karp(pattern, text)
 
 
 def test_rabin_karp_find_pattern_in_text_2():
-    """Testing for pattern matching"""
     pattern = "AAAB"
     text = "ABAAAAAB"
     assert rabin_karp(pattern, text)
 
 
 def test_rabin_karp_find_pattern_in_text_3():
-    """Testing for pattern matching"""
     pattern = "abcdabcy"
     text = "abcxabcdabxabcdabcdabcy"
     assert rabin_karp(pattern, text)
